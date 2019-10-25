b05f6e9cf1ea15c857764f1de8dca2096675ece9
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2019-10-24 22:02:30 -0400

    component templates

---
+++ src/components/contact/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './contact.css'
+
+class Contact extends Component {
+  render() {
+    return (
+      <div className="contact">
+        contact
+      </div>
+    )
+  }
+}
+
+
+export default Contact
