a51a4233864901f4caed9202b3c81f8b2969ac35
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2019-10-28 20:17:17 -0400

    images

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react'
 import './main.css'
+import biscuit from './assets/Biscuit.jpg'
+import blackbird from './assets/Blackbird.jpg'
+import cabin from './assets/Cabin.jpg'
+import dirt from './assets/Dirt.jpg'
 
 class Main extends Component {
   render() {
