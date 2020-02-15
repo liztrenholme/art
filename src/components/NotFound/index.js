f42437740c5a0f830b912524862892b9e32d4470
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2020-02-14 22:58:36 -0500

    not found component

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+// import './contact.css'
+
+class Contact extends Component {
+  render() {
+    return (
+      <div className="not-found">
+        Not found.
+      </div>
+    )
+  }
+}
+
+
+export default Contact
