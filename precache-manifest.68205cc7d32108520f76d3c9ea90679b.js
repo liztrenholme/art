99a6f80a242a22309a3daa14a6bce7440ca4b3ae
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2020-02-15 22:25:58 -0500

    Updates

---
+++ precache-manifest.68205cc7d32108520f76d3c9ea90679b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cfa24e289b2085d962c0900160b5f4a",
+    "url": "/art/index.html"
+  },
+  {
+    "revision": "1d93998d65a4a167c304",
+    "url": "/art/static/css/main.94ad0424.chunk.css"
+  },
+  {
+    "revision": "b2597bdc6433340f8fd9",
+    "url": "/art/static/js/2.a43ee24c.chunk.js"
+  },
+  {
+    "revision": "b5a01430b9928300ee798657f45fe7d4",
+    "url": "/art/static/js/2.a43ee24c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d93998d65a4a167c304",
+    "url": "/art/static/js/main.879176fc.chunk.js"
+  },
+  {
+    "revision": "b454ab7d011b9ab6cd4d",
+    "url": "/art/static/js/runtime-main.51122d5b.js"
+  },
+  {
+    "revision": "98d506dfed5e50b574fcc5df9b52612a",
+    "url": "/art/static/media/Biscuit.98d506df.jpg"
+  },
+  {
+    "revision": "6566aee13125cf9e5ef0a01a96e2e7e6",
+    "url": "/art/static/media/Blackbird.6566aee1.jpg"
+  },
+  {
+    "revision": "8d1a86292003a89bee16800f1fdb946f",
+    "url": "/art/static/media/Cabin.8d1a8629.jpg"
+  },
+  {
+    "revision": "235a5b45321936f804b2b9650ff8a4d3",
+    "url": "/art/static/media/Dirt.235a5b45.jpg"
+  },
+  {
+    "revision": "546b16ab601a746f2f218aefb9755b6a",
+    "url": "/art/static/media/Hoggy.546b16ab.jpg"
+  },
+  {
+    "revision": "e771790275901198b8bb720af6be7058",
+    "url": "/art/static/media/Magoo.e7717902.jpg"
+  },
+  {
+    "revision": "043fb7b271ccfb1e5ac70aac2d54fef7",
+    "url": "/art/static/media/Sparrow.043fb7b2.jpg"
+  },
+  {
+    "revision": "bf52df0f3e56c5f7eb31b21821678c9d",
+    "url": "/art/static/media/Time.bf52df0f.jpg"
+  },
+  {
+    "revision": "89bf5f4d63ef36bd6ca6efbb9454480a",
+    "url": "/art/static/media/viandan.89bf5f4d.jpg"
+  }
+]);
