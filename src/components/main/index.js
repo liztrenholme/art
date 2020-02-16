7b1c08e3bbabaf135b5f973fc14e63a5a09a01fe
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2020-02-16 13:50:54 -0500

    refined onMouseOver effect for selecting pictures

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -8,6 +8,7 @@ class Main extends Component {
       <div className="main">
         <h1>Liz Trenholme Art.</h1>
         <Tiles />
+        <footer>&#9400; 2000-{new Date().getFullYear()} Liz Trenholme Art | All rights reserved.</footer>
       </div>
     )
   }
--- src/components/main/tiles.js
+++ src/components/main/tiles.js
@@ -23,6 +23,7 @@ class Tiles extends Component {
     handleSelectImage = (image) => () => {
       this.setState({selectedImage: image, modalVisible: true})
     }
+    handleNoTake = (e) => e.preventDefault()
     closeModal = () => this.setState({modalVisible: false})
     render() {
       const {visibleButtons, modalVisible, selectedImage} = this.state
@@ -33,7 +34,11 @@ class Tiles extends Component {
             : null}
           {modalVisible ? 
             <div className='image-modal'>
-              <img src={selectedImage} className='modal-image' />
+              <img 
+                src={selectedImage} 
+                className='modal-image'
+                onMouseDown={this.handleNoTake}
+                onContextMenu={this.handleNoTake} />
             </div>
             : null}
           <div className='art-image-container'>
@@ -41,75 +46,134 @@ class Tiles extends Component {
               src={dirt} 
               alt='Biscuit, acrylic on canvas' 
               className='art-image'
-              onMouseOver={this.handleButtons('dirt')} />
-            {visibleButtons === 'dirt' ?
+              onMouseOver={this.handleButtons('dirt')}
+              onMouseDown={this.handleNoTake}
+              onContextMenu={this.handleNoTake}
+              onClick={this.handleSelectImage(dirt)}
+              style={visibleButtons === 'dirt' ? {opacity: 1} : {opacity: 0.9}} />
+            {/* {visibleButtons === 'dirt' ?
               <div className='image-buttons'>
                 <button onClick={this.handleSelectImage(dirt)}>View larger</button>
-              </div> : null}
+              </div> : null} */}
           </div>
           <div className='art-image-container'>
-            <img src={biscuit} alt='Biscuit, acrylic on canvas' className='art-image'
-              onMouseOver={this.handleButtons('biscuit')} />
-            {visibleButtons === 'biscuit' ?
+            <img 
+              src={biscuit} 
+              alt='Biscuit, acrylic on canvas' 
+              className='art-image'
+              onMouseOver={this.handleButtons('biscuit')}
+              onMouseDown={this.handleNoTake}
+              onContextMenu={this.handleNoTake}
+              onClick={this.handleSelectImage(biscuit)}
+              style={visibleButtons === 'biscuit' ? {opacity: 1} : {opacity: 0.9}} />
+            {/* {visibleButtons === 'biscuit' ?
               <div className='image-buttons'>
                 <button onClick={this.handleSelectImage(biscuit)}>View larger</button>
-              </div> : null}
+              </div> : null} */}
           </div>
           <div className='art-image-container'>
-            <img src={cabin} alt='Biscuit, acrylic on canvas' className='art-image'
-              onMouseOver={this.handleButtons('cabin')} />
-            {visibleButtons === 'cabin' ?
+            <img 
+              src={cabin} 
+              alt='Biscuit, acrylic on canvas' 
+              className='art-image'
+              onMouseOver={this.handleButtons('cabin')}
+              onMouseDown={this.handleNoTake}
+              onContextMenu={this.handleNoTake}
+              onClick={this.handleSelectImage(cabin)}
+              style={visibleButtons === 'cabin' ? {opacity: 1} : {opacity: 0.9}} />
+            {/* {visibleButtons === 'cabin' ?
               <div className='image-buttons'>
                 <button onClick={this.handleSelectImage(cabin)}>View larger</button>
-              </div> : null}
+              </div> : null} */}
           </div>
           <div className='art-image-container'>
-            <img src={blackbird} alt='Biscuit, acrylic on canvas' className='art-image'
-              onMouseOver={this.handleButtons('blackbird')} />
-            {visibleButtons === 'blackbird' ?
+            <img src={blackbird} 
+              alt='Biscuit, acrylic on canvas' 
+              className='art-image'
+              onMouseOver={this.handleButtons('blackbird')}
+              onMouseDown={this.handleNoTake}
+              onContextMenu={this.handleNoTake}
+              onClick={this.handleSelectImage(blackbird)}
+              style={visibleButtons === 'blackbird' ? {opacity: 1} : {opacity: 0.9}} />
+            {/* {visibleButtons === 'blackbird' ?
               <div className='image-buttons'>
                 <button onClick={this.handleSelectImage(blackbird)}>View larger</button>
-              </div> : null}
+              </div> : null} */}
           </div>
           <div className='art-image-container'>
-            <img src={mugsy} alt='Biscuit, acrylic on canvas' className='art-image'
-              onMouseOver={this.handleButtons('mugsy')} />
-            {visibleButtons === 'mugsy' ?
-              <div className='image-buttons'>
-                <button onClick={this.handleSelectImage(mugsy)}>View larger</button>
-              </div> : null}
-          </div>
-          <div className='art-image-container'>
-            <img src={viandan} alt='Biscuit, acrylic on canvas' className='art-image'
-              onMouseOver={this.handleButtons('viandan')} />
-            {visibleButtons === 'viandan' ?
+            <img 
+              src={viandan} 
+              alt='Biscuit, acrylic on canvas' 
+              className='art-image'
+              onMouseOver={this.handleButtons('viandan')}
+              onMouseDown={this.handleNoTake}
+              onContextMenu={this.handleNoTake}
+              onClick={this.handleSelectImage(viandan)}
+              style={visibleButtons === 'viandan' ? {opacity: 1} : {opacity: 0.9}} />
+            {/* {visibleButtons === 'viandan' ?
               <div className='image-buttons'>
                 <button onClick={this.handleSelectImage(viandan)}>View larger</button>
-              </div> : null}
+              </div> : null} */}
           </div>
           <div className='art-image-container'>
-            <img src={time} alt='Biscuit, acrylic on canvas' className='art-image'
-              onMouseOver={this.handleButtons('time')} />
-            {visibleButtons === 'time' ?
+            <img 
+              src={time} 
+              alt='Biscuit, acrylic on canvas' 
+              className='art-image'
+              onMouseOver={this.handleButtons('time')}
+              onMouseDown={this.handleNoTake}
+              onContextMenu={this.handleNoTake}
+              onClick={this.handleSelectImage(time)}
+              style={visibleButtons === 'time' ? {opacity: 1} : {opacity: 0.9}} />
+            {/* {visibleButtons === 'time' ?
               <div className='image-buttons'>
                 <button onClick={this.handleSelectImage(time)}>View larger</button>
-              </div> : null}
+              </div> : null} */}
           </div>
           <div className='art-image-container'>
-            <img src={hog} alt='Biscuit, acrylic on canvas' className='art-image'
-              onMouseOver={this.handleButtons('hoggy')} />
-            {visibleButtons === 'hoggy' ?
+            <img 
+              src={hog} 
+              alt='Biscuit, acrylic on canvas' 
+              className='art-image'
+              onMouseOver={this.handleButtons('hoggy')}
+              onMouseDown={this.handleNoTake}
+              onContextMenu={this.handleNoTake}
+              onClick={this.handleSelectImage(hog)}
+              style={visibleButtons === 'hoggy' ? {opacity: 1} : {opacity: 0.9}} />
+            {/* {visibleButtons === 'hoggy' ?
               <div className='image-buttons'>
                 <button onClick={this.handleSelectImage(hog)}>View larger</button>
-              </div> : null}
+              </div> : null} */}
           </div>
           <div className='art-image-container'>
-            <img src={sparrow} alt='Biscuit, acrylic on canvas' className='art-image'
-              onMouseOver={this.handleButtons('sparrow')} />
-            {visibleButtons === 'sparrow' ?
+            <img 
+              src={sparrow} 
+              alt='Biscuit, acrylic on canvas' 
+              className='art-image'
+              onMouseOver={this.handleButtons('sparrow')}
+              onMouseDown={this.handleNoTake}
+              onContextMenu={this.handleNoTake}
+              onClick={this.handleSelectImage(sparrow)}
+              style={visibleButtons === 'sparrow' ? {opacity: 1} : {opacity: 0.9}} />
+            {/* {visibleButtons === 'sparrow' ?
               <div className='image-buttons'>
                 <button onClick={this.handleSelectImage(sparrow)}>View larger</button>
-              </div> : null}
+              </div> : null} */}
+          </div>
+          <div className='art-image-container'>
+            <img 
+              src={mugsy} 
+              alt='Biscuit, acrylic on canvas' 
+              className='art-image'
+              onMouseOver={this.handleButtons('mugsy')}
+              onMouseDown={this.handleNoTake}
+              onContextMenu={this.handleNoTake}
+              onClick={this.handleSelectImage(mugsy)}
+              style={visibleButtons === 'mugsy' ? {opacity: 1} : {opacity: 0.9}} />
+            {/* {visibleButtons === 'mugsy' ?
+              <div className='image-buttons'>
+                <button onClick={this.handleSelectImage(mugsy)}>View larger</button>
+              </div> : null} */}
           </div>
         </div>
       )
