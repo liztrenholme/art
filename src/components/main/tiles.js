cadacd1ce56a541070c6441fb927184c2d1bab57
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2020-02-15 21:31:01 -0500

    onMouseOver to put image name in state

--- src/components/main/tiles.js
+++ src/components/main/tiles.js
@@ -12,65 +12,90 @@ import viandan from './assets/viandan.jpg'
 
 class Tiles extends Component {
     state = {
-        
+      visibleButtons: ''
+    }
+
+    handleButtons = (btnName) => () => {
+      this.setState({visibleButtons: btnName})
     }
     render() {
-
+      const {visibleButtons} = this.state
       return (
         <div className="tiles-container">
           <div className='art-image-container'>
-            <img src={dirt} alt='Biscuit, acrylic on canvas' className='art-image' />
-            <div className='image-buttons'>
-              <button>View larger</button>
-            </div>
+            <img 
+              src={dirt} 
+              alt='Biscuit, acrylic on canvas' 
+              className='art-image'
+              onMouseOver={this.handleButtons('dirt')} />
+            {visibleButtons === 'dirt' ?
+              <div className='image-buttons'>
+                <button>View larger</button>
+              </div> : null}
           </div>
           <div className='art-image-container'>
-            <img src={biscuit} alt='Biscuit, acrylic on canvas' className='art-image' />
-            <div className='image-buttons'>
-              <button>View larger</button>
-            </div>
+            <img src={biscuit} alt='Biscuit, acrylic on canvas' className='art-image'
+              onMouseOver={this.handleButtons('biscuit')} />
+            {visibleButtons === 'biscuit' ?
+              <div className='image-buttons'>
+                <button>View larger</button>
+              </div> : null}
           </div>
           <div className='art-image-container'>
-            <img src={cabin} alt='Biscuit, acrylic on canvas' className='art-image' />
-            <div className='image-buttons'>
-              <button>View larger</button>
-            </div>
+            <img src={cabin} alt='Biscuit, acrylic on canvas' className='art-image'
+              onMouseOver={this.handleButtons('cabin')} />
+            {visibleButtons === 'cabin' ?
+              <div className='image-buttons'>
+                <button>View larger</button>
+              </div> : null}
           </div>
           <div className='art-image-container'>
-            <img src={blackbird} alt='Biscuit, acrylic on canvas' className='art-image' />
-            <div className='image-buttons'>
-              <button>View larger</button>
-            </div>
+            <img src={blackbird} alt='Biscuit, acrylic on canvas' className='art-image'
+              onMouseOver={this.handleButtons('blackbird')} />
+            {visibleButtons === 'blackbird' ?
+              <div className='image-buttons'>
+                <button>View larger</button>
+              </div> : null}
           </div>
           <div className='art-image-container'>
-            <img src={mugsy} alt='Biscuit, acrylic on canvas' className='art-image' />
-            <div className='image-buttons'>
-              <button>View larger</button>
-            </div>
+            <img src={mugsy} alt='Biscuit, acrylic on canvas' className='art-image'
+              onMouseOver={this.handleButtons('mugsy')} />
+            {visibleButtons === 'mugsy' ?
+              <div className='image-buttons'>
+                <button>View larger</button>
+              </div> : null}
           </div>
           <div className='art-image-container'>
-            <img src={viandan} alt='Biscuit, acrylic on canvas' className='art-image' />
-            <div className='image-buttons'>
-              <button>View larger</button>
-            </div>
+            <img src={viandan} alt='Biscuit, acrylic on canvas' className='art-image'
+              onMouseOver={this.handleButtons('viandan')} />
+            {visibleButtons === 'viandan' ?
+              <div className='image-buttons'>
+                <button>View larger</button>
+              </div> : null}
           </div>
           <div className='art-image-container'>
-            <img src={time} alt='Biscuit, acrylic on canvas' className='art-image' />
-            <div className='image-buttons'>
-              <button>View larger</button>
-            </div>
+            <img src={time} alt='Biscuit, acrylic on canvas' className='art-image'
+              onMouseOver={this.handleButtons('time')} />
+            {visibleButtons === 'time' ?
+              <div className='image-buttons'>
+                <button>View larger</button>
+              </div> : null}
           </div>
           <div className='art-image-container'>
-            <img src={hog} alt='Biscuit, acrylic on canvas' className='art-image' />
-            <div className='image-buttons'>
-              <button>View larger</button>
-            </div>
+            <img src={hog} alt='Biscuit, acrylic on canvas' className='art-image'
+              onMouseOver={this.handleButtons('hoggy')} />
+            {visibleButtons === 'hoggy' ?
+              <div className='image-buttons'>
+                <button>View larger</button>
+              </div> : null}
           </div>
           <div className='art-image-container'>
-            <img src={sparrow} alt='Biscuit, acrylic on canvas' className='art-image' />
-            <div className='image-buttons'>
-              <button>View larger</button>
-            </div>
+            <img src={sparrow} alt='Biscuit, acrylic on canvas' className='art-image'
+              onMouseOver={this.handleButtons('sparrow')} />
+            {visibleButtons === 'sparrow' ?
+              <div className='image-buttons'>
+                <button>View larger</button>
+              </div> : null}
           </div>
         </div>
       )
