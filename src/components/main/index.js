c2becd891eb96da535fbf77a9b4fc4fba08f4b2f
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2020-03-21 21:26:16 -0400

    menu, function to toggle menu

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -3,10 +3,22 @@ import './main.css';
 import Tiles from './tiles';
 
 class Main extends Component {
+  state = {
+    menuOpen: false
+  }
+
+  handleToggleMenu = () => this.state.menuOpen 
+    ? this.setState({menuOpen: false}) 
+    : this.setState({menuOpen: true})
   render() {
+    const {menuOpen} = this.state;
     return (
       <div className="main">
-        <h1>Liz Trenholme Art.</h1>
+        <h1 onClick={this.handleToggleMenu}>Liz Trenholme Art.</h1>
+        {menuOpen ? 
+          (<div className='menu'>
+            <a href="https://liztrenholme.com">main site</a>
+          </div>) : null}
         <Tiles />
         <footer>&#9400; 2000-{new Date().getFullYear()} Liz Trenholme Art | All rights reserved.</footer>
       </div>
