657cd9d8896ff862540e94be9a1d269e78c1cf82
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2020-03-21 22:10:40 -0400

    buildout and styling for contact component

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -5,7 +5,16 @@ class Contact extends Component {
   render() {
     return (
       <div className="contact">
-        contact
+        <form
+          className='contact-form'
+          action="https://formspree.io/liztrenholme@gmail.com" 
+          method="POST">
+          <input className="text-input" placeholder="First Name" required type="text" name="first_name" />
+          <input className="text-input" placeholder="Last Name" type="text" name="last_name" />
+          <input className="text-input" placeholder="Your Email Address" required type="email" name="_replyto" />
+          <textarea className="text-input" required rows="10" cols="38" name="message" placeholder="Message..." width="50%" height="50%"></textarea>
+          <input type="submit" className='submit-button' value="Send" />
+        </form>
       </div>
     );
   }
